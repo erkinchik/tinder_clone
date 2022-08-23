16decc26ff51eaefd90623e4dea46530a954af53
erkinchik <erkin.berdibaev@gmail.com>
2022-08-23 16:28:15 +0600

    add login screen, fix tailwind

--- screens/LoginScreen.jsx
+++ screens/LoginScreen.jsx
@@ -1,14 +1,44 @@
-import React from "react";
+import React, { useLayoutEffect } from "react";
 
-import { View, Text, Button } from "react-native";
+import {
+  View,
+  Text,
+  Button,
+  ImageBackground,
+  TouchableOpacity,
+} from "react-native";
 import useAuth from "../hooks/useAuth";
+import { useNavigation } from "@react-navigation/core";
+import tw from "tailwind-rn";
 
 const LoginScreen = () => {
   const { signInWithGoogle, loading } = useAuth();
+  const navigation = useNavigation();
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerShown: false,
+    });
+  }, []);
   return (
-    <View>
-      <Text>{loading ? "loading..." : "login"}</Text>
-      <Button title={"login"} onPress={signInWithGoogle} />
+    <View style={tw("flex-1 ")}>
+      <ImageBackground
+        resizeMode="cover"
+        style={tw("flex-1")}
+        source={{ uri: "https://tinder.com/static/tinder.png" }}
+      >
+        <TouchableOpacity
+          style={[
+            tw("absolute bottom-40 w-52 bg-white p-4 rounded-2xl "),
+            { marginHorizontal: "25%" },
+          ]}
+          onPress={signInWithGoogle}
+        >
+          <Text style={tw("text-center font-semibold")}>
+            Sign in & get swiping
+          </Text>
+        </TouchableOpacity>
+      </ImageBackground>
     </View>
   );
 };

d9a32c82e0fe636d974668a34de7d6db80aa7db5
Erkin <erkin.berdibaev@gmail.com>
2022-08-23 09:52:19 +0300

    fix

--- hooks/useAuth.js
+++ hooks/useAuth.js
@@ -1,5 +1,4 @@
 import React, { createContext, useContext } from "react";
-import { View, Text } from "react-native";
 import * as Google from "expo-google-app-auth";
 
 const AuthContext = createContext({});
